e2ef7e09c87ebe14d99344c8d26e4f5f48374c5b
vlaya8 <vlaya8@protonmail.com>
2020-06-13 15:38:44 +0200

    Migrate to add admin, writer and reader roles

---
+++ tags/migrations/0020_auto_20200613_1531.py
@@ -0,0 +1,33 @@
+from django.db import migrations
+from django.db import models
+from tags.models import GroupRole
+
+def add_roles(apps, schema_editor):
+
+    GroupRole.objects.create(name="admin",
+                         manage_users=True,
+                         manage_entries=True)
+
+    GroupRole.objects.create(name="writer",
+                         manage_users=False,
+                         manage_entries=True)
+
+    GroupRole.objects.create(name="reader",
+                         manage_users=False,
+                         manage_entries=False)
+
+def rev_add_roles(apps, schema_editor):
+
+    GroupRole.objects.filter(name="admin").delete()
+    GroupRole.objects.filter(name="writer").delete()
+    GroupRole.objects.filter(name="reader").delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tags', '0019_grouprole_name'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_roles, rev_add_roles),
+    ]
