68461cbcb939ff9f83659dbf2ad0f1a1cac3ddc6
vlaya8 <vlaya8@protonmail.com>
2020-06-13 18:54:28 +0200

    Fix import oversight

--- tags/signals.py
+++ tags/signals.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from tags.models import TreeUserGroup, Role
+from tags.models import TreeUserGroup, Role, Member
 
 # Each user must have a group with only them in it
 @receiver(post_save, sender=User)
