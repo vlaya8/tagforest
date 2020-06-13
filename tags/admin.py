f215e9317560b5ff887c29eebbfbb2cb33c0cf52
vlaya8 <vlaya8@protonmail.com>
2020-06-13 18:56:56 +0200

    Add group models to admin

--- tags/admin.py
+++ tags/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from tags.models import Tree,Entry,Tag
+from tags.models import Tree, Entry, Tag
+from tags.models import TreeUserGroup, Member, Role
 
-# Register your models here.
+# Trees
+
 admin.site.register(Tree)
 admin.site.register(Entry)
 admin.site.register(Tag)
+
+# Groups
+
+admin.site.register(TreeUserGroup)
+admin.site.register(Member)
+admin.site.register(Role)
