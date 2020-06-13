fe6d51867bea18598265574d063cf6b09471cad6
vlaya8 <vlaya8@protonmail.com>
2020-06-13 16:01:43 +0200

    Add signal to create a groupe with single member on user creation

---
+++ tags/signals.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from tags.models import Group
+
+# Each user must have a group with only them in it
+@receiver(post_save, sender=User)
+def create_group(sender, instance, created, **kwargs):
+    if created:
+        admin_role = GroupRole.objects.filter(name="admin")
+        member = Member.objects.create(user=instance, role=admin_role)
+        group = Group.objects.create(name=instance.name,
+                                     single_member=True)
+        group.members.add(member)
+
+
