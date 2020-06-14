e14c34061774915a4293fd65ca2fb36122cbce5d
vlaya8 <vlaya8@protonmail.com>
2020-06-14 15:10:46 +0200

    Add signals to synchronize profile with user

--- tags/signals.py
+++ tags/signals.py
@@ -13,4 +13,11 @@ def create_group(sender, instance, created, **kwargs):
                                      single_member=True)
         member = Member.objects.create(user=instance, role=admin_role, group=group)
 
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    instance.profile.Save()
